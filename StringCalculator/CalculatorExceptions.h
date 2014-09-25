ac94ed1a8da5e251b35d54a3d9e4759c8512311d
Artem Titov <seter17@gmail.com>
2014-09-25 13:03:38 +0400

    Added file with exceptions

---
+++ StringCalculator/CalculatorExceptions.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <string>
+#include <exception>
+
+
+class CalculatorException : std::exception {
+public:
+	CalculatorException(int index) {
+		this->index = index;
+	};
+
+	virtual  ~CalculatorException() {};
+
+	virtual const char* what() const throw() {
+		FormatMessage();
+		return message.c_str();
+	}
+
+protected:
+	virtual std::string GetMessage() const { return "Calculator failed"; }
+
+private:
+	int index;
+	mutable std::string message;
+	const void FormatMessage() const {
+		message = GetMessage();
+		if (index != -1) {
+			char buff[100];
+			sprintf_s(buff, " at %d", index);
+			message.append(buff);
+		}
+	}
+};
+
+class DivisionByZeroException : public CalculatorException {
+public:
+	DivisionByZeroException() : CalculatorException(-1) {}
+	virtual ~DivisionByZeroException() {}
+protected:
+	virtual std::string GetMessage() const { return "Division by zero"; }
+};
+
+class ParseException: public CalculatorException {
+public:
+	ParseException(int index) : CalculatorException(index) {};
+	virtual ~ParseException() {};
+
+protected:
+	virtual std::string GetMessage() const { return "Wrong expression format"; }
+};
