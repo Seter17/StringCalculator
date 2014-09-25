74118da3a1d0cf077b3bf8df1390a626bc8e7cd1
Artem Titov <seter17@gmail.com>
2014-09-25 13:51:15 +0400

    Added command line support

--- StringCalculator/Main.cpp
+++ StringCalculator/Main.cpp
@@ -3,22 +3,39 @@
 #include "Calculator.h"
 
 
-int main(std::string* args){
+int main(int argc,      // Number of strings in array argv
+		char *argv[],   // Array of command-line argument strings
+		char *envp[]) {
 
-	std::cout << "Wtf, man?!" << std::endl;
-    int start = 0;
-    //std::string test = "23.5 - 8 * (4 + 7)";
-    //std::string test = "2 + 5*(3 - (6 + 5) - 3 * 2) -7";
-    //std::string test = "-2 + sin(30)";
-    std::string test = "2 + 5*(3 - (6 + sin(30)/0) - 3 * 2) -7";
-    
-	std::string errorMessage;
-	float value =  Calculator::Evaluate(test, errorMessage);
+	if (argv == 0)
+	{
+		std::cout << "Wtf, man?!" << std::endl;
+	}
+	
+	if (argv[1] == "test") {
+		//launch tests
+		std::cout << "Launching tests" << std::endl;
+	}
+	else {
+		int start = 0;
+		//std::string test = "23.5 - 8 * (4 + 7)";
+		//std::string test = "2 + 5*(3 - (6 + 5) - 3 * 2) -7";
+		//std::string test = "-2 + sin(30)";
+		//std::string test = "2 + 5*(3 - (6 + sin(30)/0) - 3 * 2) -7";
+		std::string expression;
+		for (int i = 1; i < argc; ++i)
+		{
+			expression.append(argv[i]);
+		}
+		std::cout << expression << std::endl;
+		std::string errorMessage;
 
-	if (errorMessage.empty())
-		std::cout << value;
-	else
-		std::cout <<errorMessage;
-	std::cin.get();
+		float value = Calculator::Evaluate(expression, errorMessage);
+
+		if (errorMessage.empty())
+			std::cout << value;
+		else
+			std::cout << errorMessage;
+	}
 	return 0;
 }
